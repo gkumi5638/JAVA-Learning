902e0e39eae5ca704e7cdd8badcc66d5c2d0ff36
kumi <>
2019-05-03 17:39:44 +0900

    コメントの追加

--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
+/*　コメントの追加
+    作成日 2019/5/3
+*/
 public class Main {
+
+	//ここからMainメソッド
 	public static void main(String[] args){
 		System.out.println("hello,world");
 		System.out.println(31 + 31);
