4c4b92d103ecc546687e0977a9f07413e37761dc
kumi <t.kumi5638@gmail.com>
2019-05-03 20:47:08 +0900

    if文

--- Main.java
+++ Main.java
@@ -2,13 +2,15 @@
     作成日 2019/5/3
 */
 public class Main {
-
-	//ここからMainメソッド
 	public static void main(String[] args){
-		int age = 20;       			//変数ageを20で初期化
-		System.out.println("私の年齢は" + age);
-		age = 31;				//変数ageに値を再代入
-		System.out.println("年齢を" + age + "へ変更しました。"); 
+	  boolean tenki = true;
+	  if(tenki == true){
+	    System.out.println("洗濯をします");
+	    System.out.println("散歩に行きます");
+	  } else {
+	    System.out.println("DVDを見ます");
+	  }
+
 	}
 }
 
