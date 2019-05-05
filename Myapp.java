105628fb38ed98bbc722e1c9bc8f9a7290fcb858
kumi <t.kumi5638@gmail.com>
2019-05-05 23:29:06 +0900

    #5 データ型の使い方

--- Myapp.java
+++ Myapp.java
@@ -1,12 +1,24 @@
 public class Myapp{ 
 
 	public static void main(String[] args){
-	//変数
-	//型　変数名
+	  // 文字
+	  char a = 'a';
 
-	//String msg;
-	//msg = "Hello World";
-	  String msg = "Hello World";
+	  // 整数 byte short int long
+	  int x = 10;
+	  long y = 5555555555L;
+
+	  // 浮動小数点数 float double	  
+	  double d = 23423.344;
+	  float f = 32.33F;
+
+	  // 論理値
+	  boolean flag = true; //false
+
+	  // 文字列
+	  // \n 改行
+	  // \t	タブ
+	  String msg = "Hello Wo\nrld again\t!";
 	  System.out.println(msg);
 	}	
 }
