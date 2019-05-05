3aace47297cada9a1243eb666cccf67a24bc1982
kumi <t.kumi5638@gmail.com>
2019-05-05 23:15:57 +0900

    #4変数/ドットインストールから写経

---
+++ Myapp.java
@@ -0,0 +1,12 @@
+public class Myapp{ 
+
+	public static void main(String[] args){
+	//変数
+	//型　変数名
+
+	//String msg;
+	//msg = "Hello World";
+	  String msg = "Hello World";
+	  System.out.println(msg);
+	}	
+}
